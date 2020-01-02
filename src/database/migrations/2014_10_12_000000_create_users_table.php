a616fb734431038472515573c7de171de94f8c53
kawa1228 <sxa.1105bo@gmail.com>
2020-01-02 18:21:08 +0900

    user_tableのマイグレーションファイルを修正

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -15,12 +16,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name')->comment('名前');
+            $table->string('employee_id')->comment('社員ID')->nullable();
+            $table->integer('status')->comment('ステータス')->default(User::STATUS_VALID);
+            $table->integer('team_id')->comment('チームID')->nullable();
+
+            $table->string('google_user_id')->comment('GoogleユーザーID')->nullable();
+            $table->string('email')->comment('メールアドレス')->nullable();
+            $table->string('icon_url')->comment('アイコンURL')->nullable();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->text('created_by')->nullable();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->text('updated_by')->nullable();
         });
     }
 
